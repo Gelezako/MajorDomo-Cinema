7145ef59715cb5346280fc009e1edfef379db164
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-03-07 21:27:40 +0700

    Add files via upload

---
+++ modules/Cinema/Cinema.class.php
@@ -0,0 +1,194 @@
+<?php
+/**
+* Cinema 
+* @package project
+* @author Alex Sokolov <admin@gelezako.com>
+* @copyright Alex Sokolov http://blog.gelezako.com (c)
+* @version 0.1 (wizard, 11:02:06 [Feb 06, 2017])
+*/
+//
+//
+class Cinema extends module {
+/**
+* Cinema
+*
+* Module class constructor
+*
+* @access private
+*/
+  public function Cinema() {
+  $this->name="Cinema";
+  $this->title="Фильмы в кинотеатре";
+  $this->module_category="<#LANG_SECTION_APPLICATIONS#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+ public function saveParams($data=0) {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+  public function getParams() {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+ public function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+
+   protected function SetAutoUpdate()
+   {
+      
+   }
+
+public function admin(&$out) {
+
+global $URL;
+if(isset($URL)){
+        $this->SaveData($URL);
+    }
+}
+
+$this->get_settings($out,$region);
+}
+
+
+public function get_settings(&$out,$region)
+{
+    $out["URL"] = $URL;
+
+}
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+ public function usual(&$out) {
+ $this->admin($out);
+}
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ public function install($data='') {
+ $className = 'Cinema'; //имя класса
+ $objectName = array('City');//имя обьектов
+ $objDescription = array('Фильмы в кинотеатре');
+ $rec = SQLSelectOne("SELECT ID FROM classes WHERE TITLE LIKE '" . DBSafe($className) . "'");
+ 
+    if (!$rec['ID']) {
+        $rec = array();
+        $rec['TITLE'] = $className;
+        $rec['DESCRIPTION'] = $objDescription;
+        $rec['ID'] = SQLInsert('classes', $rec);
+    }
+    for ($i = 0; $i < count($objectName); $i++) {
+        $obj_rec = SQLSelectOne("SELECT ID FROM objects WHERE CLASS_ID='" . $rec['ID'] . "' AND TITLE LIKE '" . DBSafe($objectName[$i]) . "'");
+        if (!$obj_rec['ID']) {
+            $obj_rec = array();
+            $obj_rec['CLASS_ID'] = $rec['ID'];
+            $obj_rec['TITLE'] = $objectName[$i];
+            $obj_rec['DESCRIPTION'] = $objDescription[$i];
+            $obj_rec['ID'] = SQLInsert('objects', $obj_rec);
+        }
+    }
+	addClassProperty('City', 'URL', 'include_once(DIR_MODULES."Cinema/Cinema.class.php");');
+	parent::install();
+ }
+
+ public function uninstall()
+   {
+      SQLExec("delete from pvalues where property_id in (select id FROM properties where object_id in (select id from objects where class_id = (select id from classes where title = 'Cinema')))");
+      SQLExec("delete from properties where object_id in (select id from objects where class_id = (select id from classes where title = 'Cinema'))");
+      SQLExec("delete from objects where class_id = (select id from classes where title = 'Cinema')");
+      SQLExec("delete from classes where title = 'Cinema'");
+      
+      parent::uninstall();
+   }
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgRmViIDA2LCAyMDE3IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
